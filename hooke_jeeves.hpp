56bdb455caf7ed1ea6d46de0f144772b25bd51cb
letterXi <qwzxaser2015@gmail.com>
2026-03-23 15:52:10 +0300

    hooke jeeves done

---
+++ hooke_jeeves.hpp
@@ -0,0 +1,71 @@
+#ifndef HOOKE_JEEVES_HPP
+#define HOOKE_JEEVES_HPP
+
+#include "my_math/vector.hpp"
+#include <functional>
+#include <iostream>
+
+namespace Opt {
+using Vector = MyMath::Vector<double>; // (x1, x2, x3, ..., xn)
+using Vectors = MyMath::Vector<Vector>;
+using FunctionND = std::function<double(const Vector&)>; // f(x1, ..., xn) -> R
+using Function1D = std::function<double(double)>;        // f(x) -> R
+
+// Golden section search
+inline double golden_section_search(Function1D f, double a, double b, double tolerance) {
+    //.......
+    return 0.0;
+}
+
+// Create basis vectors
+inline Vectors create_d(size_t n) {
+    Vectors D(n);
+    for (size_t i = 0; i < n; i++) {
+        Vector temp(n, 0.0);
+        temp[i] = 1.0;
+        D[i] = temp;
+    }
+    return D;
+}
+
+inline Vector hooke_jeeves(FunctionND f, Vector x, double eps) {
+    //============================================ Initial Stage ============================================
+    size_t n = x.size();
+    Vectors D = create_d(n); // e_j
+
+    Vector new_x;
+    Vector d;
+    double tau;
+
+    Vector y = x;
+    size_t iter = 1;
+
+    //============================================ Main Stage ============================================
+    while (true) {
+        //====================== Step 1 ======================
+        for (size_t j = 0; j < n; j++) {
+            tau = golden_section_search([&](double t) { return f(y + t * D[j]); }, -10, 10, eps);
+            y = y + tau * D[j];
+        }
+        new_x = y;
+        // euclide norm
+        double residual = norm(new_x - x, 2);
+        std::cout << iter << ' ' << residual << std::endl;
+
+        if (residual <= eps) {
+            x = new_x;
+            break;
+        }
+        //====================== Step 2 ======================
+        d = new_x - x;
+        x = new_x;
+        tau = golden_section_search([&](double t) { return f(x + t * d); }, -10, 10, eps);
+        y = x + tau * d;
+        iter++;
+    }
+    std::cout << y << std::endl;
+    return x;
+}
+
+} // namespace Opt
+#endif
